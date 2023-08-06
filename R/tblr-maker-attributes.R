eaa903b19ae5f6e867ccf426ec52787efe2b7d6d
turbanisch <fetu@gmx.de>
2023-08-06 12:08:24 +0200

    add print method

--- R/tblr-maker-attributes.R
+++ R/tblr-maker-attributes.R
@@ -128,6 +128,10 @@ tblr_as_latex <- function(x) {
   knitr::asis_output(out)
 }
 
+print.tblr <- function(x) {
+  writeLines(tblr_as_latex(x))
+}
+
 knit_print.tblr <- function(x, ...) {
   x <- tblr_as_latex(x)
   knitr::knit_print(x, ...)
