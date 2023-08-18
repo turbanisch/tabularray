f83a70656b1fef2f5919ccc2e6e5019e6ee18875
turbanisch <fetu@gmx.de>
2023-08-19 01:13:45 +0200

    Pick most flexible version
    Function accepts named vector or formula objects in the dots ... argument. In the case of a formula, the LHS is interpreted via tidy-select.

--- R/set_boxhead_property.R
+++ R/set_boxhead_property.R
@@ -1,75 +1,16 @@
-# multiple implementations below
-# different interface (with dots or single argument `cols`; as named list or formula in dots)
-# different functionality (tidy-select or not)
+# example -----------------------------------------------------------------
 
-# single "column" argument with tidy-select -------------------------------
-
-set_boxhead_property <- function(df, cols, property, replacement) {
-  # find position/names of variables selected via tidy-select
-  expr <- rlang::enquo(cols)
-  target_col_positions <- tidyselect::eval_select(expr, data = df)
-  target_col_names <- names(target_col_positions)
-  
-  # replace value in the property-column in rows corresponding to those variables
-  boxhead <- attr(df, "boxhead")
-  boxhead[boxhead$variable %in% target_col_names, property] <- replacement
-  
-  attr(df, "boxhead") <- boxhead
-  return(df)
-}
-
-# columns + value as named list in dots argument (no tidy-select) ----------
-
-set_boxhead_property <- function(df, property, ...) {
-  
-  kwargs <- rlang::list2(...)
-  
-  target_col_names <- names(kwargs)
-  replacement <- unname(kwargs)
-  
-  boxhead <- attr(df, "boxhead")
-  
-  
-  # replace value in the property-column in rows corresponding to those variables
-  for (i in seq_along(kwargs)) {
-    boxhead[boxhead$variable %in% target_col_names[i], property] <- replacement[i]
-  }
-  
-  attr(df, "boxhead") <- boxhead
-  return(df)
-}
-
-set_alignment <- function(df, ...) {
-  set_boxhead_property(df = df, property = "alignment", ...)
-}
+# df <- tibble(
+#   continent = c("Europe", "Asia", "Asia", "Europe", "Asia"),
+#   country = c("Germany","China", "Afghanistan","France","Taiwan"),
+#   value = c(0.17, 0.23, 11.3, 17, 2.4)
+# )
+# 
+# tblr(df) |> 
+#   set_alignment(value = "X", starts_with("c") ~ "c")
 
 
-# columns + value as formulas in dots list (with tidy-select) -------------
-
-set_boxhead_property <- function(df, property, ...) {
-  
-  kwargs <- rlang::list2(...)
-  boxhead <- attr(df, "boxhead")
-  
-  for (i in seq_along(kwargs)) {
-    
-    expr <-  rlang::f_lhs(kwargs[[i]])
-    target_col_positions <- tidyselect::eval_select(expr, data = df)
-    target_col_names <- names(target_col_positions)
-    
-    replacement <- kwargs[[i]] |> rlang::f_rhs()
-    
-    # replace value in the property-column in rows corresponding to those variables
-    boxhead[boxhead$variable %in% target_col_names, property] <- replacement
-  }
-  
-  attr(df, "boxhead") <- boxhead
-  return(df)
-}
-
-
-
-# columns + value as named list or formula in dots argument (with  --------
+# main function -----------------------------------------------------------
 
 set_boxhead_property <- function(df, property, ...) {
   
@@ -100,11 +41,6 @@ set_boxhead_property <- function(df, property, ...) {
 
 # wrapper functions -------------------------------------------------------
 
-set_alignment <- function(df, cols, align) {
-  set_boxhead_property(df = df, cols = {{cols}}, property = "alignment", replacement = align)
+set_alignment <- function(df, ...) {
+  set_boxhead_property(df = df, property = "alignment", ...)
 }
-
-
-# apply combined ----------------------------------------------------------
-
-tblr(df) |> set_alignment(value = "X", starts_with("c") ~ "c")
